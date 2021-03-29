768a979e3aa970fda48a86c8dcb38a7a363ecbfa
Paul Plew <thepaulplew@gmail.com>
2021-03-29 19:57:20 -0400

    Add comments to main.js

--- circle-move/js/main.js
+++ circle-move/js/main.js
@@ -1,3 +1,4 @@
+// Global Variables
 let mouseCircle;
 let oldCircles = [];
 let colors = [
@@ -11,25 +12,36 @@ let colors = [
 '#859900'
 ];
 
+// setup function
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noStroke();
 	frameRate(60);
-
+	
+	// start the circle that follows the mouse in the middle 
+	// of the page
 	mouseCircle = new Circle(width / 2, height / 2);
 }
 
+// draw function called each frame
 function draw() {
+	// if the window size has changed from the previous frame set
 	if (windowWidth > width || windowHeight > height 
 		|| windowWidth < width || windowHeight < height) {
 		createCanvas(windowWidth, windowHeight);
 	} 
+	
 	background('#FDF6E33A');
-	if (oldCircles.length === 0) {
+	
+	// if the screen is erasing and the length of the old
+	// circles is 0, stop erasing
+	if (erasing && oldCircles.length === 0) {
 		erasing = false;
 		document.title = "Click Me!"
+	// erase the last circle every 10 frames
 	} else if (erasing) {
 		if (frameCount % 10 === 0) {
+			// change the title from `Erasing' to `Erasing...'
 			if (document.title === "Erasing...") {
 				document.title = "Erasing"
 			} else {
@@ -38,9 +50,12 @@ function draw() {
 		oldCircles.shift();
 		}
 	}
+	
+	// draw all the circles
 	drawCircles();
 }
 
+// class to represent a circle that can be drawn
 class Circle {
 	constructor(positionX, positionY) {
 		this.x = positionX;
@@ -52,24 +67,31 @@ class Circle {
 		this.c2 = color(colors[(int)(Math.random() * colors.length)]);
 	}
 
+	returns the X position
 	get positionX() {
 		return this.x;
 	}
 
+	// returns the Y position
 	get positionY() {
 		return this.y;
 	}
 
 	update() {
 		this.life ++;
+		
+		// switch the boolean that represents increasing
 		if (this.life % 20 === 0) {
 			this.increasing = !this.increasing;
 		}
  		
+		// set the first color to the second pick a new color
  		if (this.life % 100 === 0) {
  			this.c1 = this.c2;
  			this.c2 = color(colors[(int)(Math.random() * colors.length)]);
  		}
+		
+		// resize the circle
 		if (this.increasing) {
 			this.radius += 0.5;
 		} else {
@@ -77,41 +99,55 @@ class Circle {
 		}
 	}
 
+	// change the X and Y position of this circle
 	set(newX, newY) {
 		this.x = newX;
 		this.y = newY;
 	}
-
+	
+	// draw this circle
 	draw() {
+		// liner interpolate the color from the old color
+		// to the new color
 		let inter = map(this.life % 100, 0, 99, 0, 1);
 		let c = lerpColor(this.c1, this.c2, inter);
 		fill(c);
+	
+		// draw this circle
 		push();
-      	translate(this.x, this.y);
-      	circle(0, 0, this.radius);
-     	pop();
+      		translate(this.x, this.y);
+      		circle(0, 0, this.radius);
+     		pop();
 	}
 }
 
+// draws and updates all the circles that exist
 function drawCircles() {
+	// lerp the location of the mouse circle so it moves smoothly
 	mouseCircle.set(lerp(mouseCircle.positionX, mouseX, 0.05), lerp(mouseCircle.positionY, mouseY, 0.05));
 	mouseCircle.update();
 	mouseCircle.draw();
 
+	// update and draw all the old circles
 	for (let i = 0; i < oldCircles.length; i++) {
 		oldCircles[i].update();
 		oldCircles[i].draw();
 	}
 }
 
+// called when the mouse is clicked
 function mouseClicked() {
+	// add a new circle at the current position of the mouse circle
 	oldCircles.push(new Circle(mouseCircle.positionX, mouseCircle.positionY));
 }
 
+// called when a key is pressed
 function keyPressed() {
+	// if 'r' is pressed
 	if (keyCode === 82) {
 		document.title = "Erasing."
 		erasing = true;
+	// remove the oldest circle
 	} else {
 		oldCircles.shift();
 	}
