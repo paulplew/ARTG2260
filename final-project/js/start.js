ceef7cd3ad8ba10357aeeea4ba1f95d69cdca0d3
Paul Plew <thepaulplew@gmail.com>
2021-04-20 18:53:03 -0400

    add button to instantly generate maze

--- final-project/js/start.js
+++ final-project/js/start.js
@@ -1,11 +1,13 @@
-let startButton;
+let animateButton;
 let title;
 let startHasBeenSetup = false;
 
 function startSetup() {
 	startHasBeenSetup = true;
-	startButton = new Button(width / 2, 2 * height / 3, 300, 100);
-	title = new Button(width / 2, height / 4, 500, 150);
+	animateButton = new Button(width / 2, (2 * height / 3) - 75, 300, 100);
+    generateButton = new Button(width / 2, (2 * height / 3) + 75, 300, 100);
+
+	title = new Button(width / 2, height / 4, 500, 200);
 }
 
 function startDraw() {
@@ -16,21 +18,35 @@ function startDraw() {
 	}
 
     textSize(32);
-    if (startButton.mouseCollide()) {
+    if (animateButton.mouseCollide()) {
         highlightColor = '#FEB95FBB';
-        startButton.overlayText("START");
+        animateButton.overlayText("Animate");
     	highlightColor = '#FEB95F'
     } else {
-    	startButton.overlayText("START");
+    	animateButton.overlayText("Animate");
     }
 
+    textSize(32);
+    if (generateButton.mouseCollide()) {
+        highlightColor = '#FEB95FBB';
+        generateButton.overlayText("Generate");
+        highlightColor = '#FEB95F'
+    } else {
+        generateButton.overlayText("Generate");
+    }
+    
+
     textSize(75);
-    title.overlayText("MAZE")
+    title.overlayText("Maze\nMaker")
 }
 
 function startClicked() {
-	if (startButton.mouseCollide()) {
+	if (animateButton.mouseCollide()) {
 		startState = false;
 		playState = true;
-	}
+	} else if (generateButton.mouseCollide()) {
+        startState = false;
+        animate = false;
+        playState = true;
+    }
 }
