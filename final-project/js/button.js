4e41a3bb8b6cb6c7255333e9f1e7dbbe0f1f5041
Paul Plew <plew.p@northeastern.edu>
2021-04-29 17:53:32 -0400

    add color selection for text overlay

--- final-project/js/button.js
+++ final-project/js/button.js
@@ -1,22 +1,28 @@
-function Button(x, y, w, h) {
+// represents a button
+function Button(x, y, w, h, color) {
 	this.x = x;
 	this.y = y;
 	this.w = w;
 	this.h = h;
+	this.color = color;
 
+	// draws a rectangle for this button
 	this.draw = function() {
 		noStroke();
-		fill(highlightColor);
+		fill(this.color);
 		rect(this.x, this.y, this.w, this.h, 10, 10, 10, 10);
 	};
 
-	this.overlayText = function(string) {
+	// overlays text over the button background with the given
+	// size and color
+	this.overlayText = function(string, size, color) {
 		this.draw();
-
-		fill(wallColor);
+		textSize(size);
+		fill(color);
 		text(string, this.x, this.y);
 	}
 
+	// determines if the mouse is over this button
 	this.mouseCollide = function() {
 		return (mouseX >= this.x - this.w / 2 
 			&& mouseX <= this.x + this.w / 2
